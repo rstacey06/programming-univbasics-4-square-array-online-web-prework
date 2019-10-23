dcfe02b3e87e1d16f739650d326c0915925bcc3e
burbage stacey <rstacey06@gmail.com>
2019-10-23 18:14:07 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,5 @@
 def square_array(array)
-
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
